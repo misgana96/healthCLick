b22a96b91c7d6fe6e9e3371567ef3409e20ab461
Misgana96 <misganayoseph@gmail.com>
2020-03-13 15:18:37 +0300

    About component

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 function About(props) {
 	return(
-		<p>Hello from About page</p>
+		<div className="container">
+			<div className="row row-content">
+				<div className="col-12 col-sm-5">
+					<h3>Our Story</h3>
+					<p>HealthClick is a home of health. We provide any health content from physical to psychological
+					. we care for your health so that we bring each and every health content from different proffesionals.
+					 </p>
+				</div>
+				<div className="col-12 col-sm-6 offset-1">
+					<img src="images/blackDoc.png" alt = "blackDoc"/>
+				</div>
+			</div>
+		</div>
 		);
 }
 
